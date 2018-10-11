445bc878b832e46548be45ae54bbd939a12253b4
YR <43808189+Banana-ftw@users.noreply.github.com>
2018-10-11 23:05:21 +0530

    Add files via upload

--- nc_knocker.sh
+++ nc_knocker.sh
@@ -26,16 +26,16 @@ sleep 2.5s
 echo "[U] Usage: Modify list.txt to a path/file of your choosing."
 echo " "
 sleep 2.5s
-echo "[U] Usage: Modify output.txt to a path/file of your choosing."
-echo " "
-sleep 2.5s
 echo "[E] Example: /root/Desktop/target.txt will replace list.txt"
 echo " "
 sleep 2.5s
-echo "[E] Example: /root/Desktop/result.txt will replace output.txt"
+echo "[i] Info: Outputs will be created automatically in the name of the IP.txt"
 echo " "
 sleep 2.5s
-echo "[W] WARNING!! Exit the Script Now if you have forgotten to modify list.txt & output.txt"
+echo "[E] Example: 192.168.88.74.txt 192.168.88.75.txt or available in the list.txt"
+echo " "
+sleep 2.5s
+echo "[W] WARNING!! Exit the Script Now if you have forgotten to modify list.txt"
 echo " "
 sleep 5s
 echo "[GETTING READY] The Script Will Start in 5 Seconds"
@@ -43,6 +43,6 @@ sleep 5s
 
 for ip in $(cat list.txt); do				#Modify list.txt
 for i in $(seq 1 65535); do
-nc -nv -z $ip $i |& tee -a output.txt;		#Modify Output.txt
+nc -nv -z $ip $i |& tee -a $ip.txt;
 done
 done
